9cde575c94d8b729047780765b1e9508a9278e30
JayleenZ <JayleenZ@Macbooks-MacBook-Pro-4.local>
2016-12-07 21:28:29 -0500

    use state setting instead of toggle class

--- src/components/service-list-item.js
+++ src/components/service-list-item.js
@@ -3,45 +3,31 @@ import React, {Component} from 'react';
 export default class ServiceListItem extends Component {
 	constructor(props) {
 		super(props);
+		this.state = { selected: false };
 		this.handleClickBindRoot = this.handleClick.bind(this);
 	}
 
-	componentDidMount(e) {
+	componentDidMount() {
 		if (this.props.index == 0) {
-			const elem = document.querySelector(".service-text-ctnr:first-child");
-			this.doSelection(elem);
-		}
+			this.handleServiceChange();
+		}	
 	}
 
-	handleClick(e) {
-		this.doSelection(e.target);
+	handleClick() {
+		this.handleServiceChange();
 	}
 
-	doSelection(elem) {
-		let deltaValue;
-		if (elem.classList.contains('service-text-ctnr')) {
-			deltaValue = this.toggleClass(elem, 'selected');
-			this.props.onServiceChange(deltaValue);
-		}
-	}
-
-	toggleClass(elem, className) {
-		let deltaValue = 0; 
-		if (elem.classList.contains(className)) {
-			elem.classList.remove(className);
-			deltaValue = - this.props.value;
-		} else {
-			elem.classList.add(className);
-			deltaValue = this.props.value;
-		}
-		return deltaValue;
+	handleServiceChange() {
+		const deltaValue = (this.state.selected) ? (- this.props.value) : this.props.value;
+		this.setState({ selected: !this.state.selected });
+		this.props.onServiceChange(deltaValue);
 	}
 
 	render() {
 		return (
-			// stupid error arrow function to call, dont rewrite on click function
+			// stupid error: arrow function to call, dont rewrite on click function
 			<li onClick={(e) => this.handleClickBindRoot(e)} className="service-item">
-				<div className="service-text-ctnr">
+				<div className={`service-text-ctnr ${ (this.state.selected) ? "selected" : ""}`}>
 					<span className="service-name">{this.props.name}</span>
 					<span className="service-value">${this.props.value}</span>
 				</div>
